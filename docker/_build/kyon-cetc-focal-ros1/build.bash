80131f29d009c98ee47e2c56e4e0cdfbb234252a
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-05-09 13:06:27 +0200

    wip

--- docker/_build/kyon-cetc-focal-ros1/build.bash
+++ docker/_build/kyon-cetc-focal-ros1/build.bash
@@ -3,19 +3,20 @@ set -e
 
 export USER_NAME=kyon_ros1
 export USER_ID=1000
-
 export KERNEL_VER=5
-docker compose build 
-docker tag kyon-cetc-focal-ros1-base hhcmhub/kyon-cetc-focal-ros1-base:latest
-docker tag kyon-cetc-focal-ros1-xeno hhcmhub/kyon-cetc-focal-ros1-xeno-v$KERNEL_VER:latest
-docker tag kyon-cetc-focal-ros1-locomotion hhcmhub/kyon-cetc-focal-ros1-locomotion:latest
+
+TAGNAME=v1.0.0
+docker compose build
+docker tag kyon-cetc-focal-ros1-base hhcmhub/kyon-cetc-focal-ros1-base:$TAGNAME
+docker tag kyon-cetc-focal-ros1-xeno hhcmhub/kyon-cetc-focal-ros1-xeno-v$KERNEL_VER:$TAGNAME
+docker tag kyon-cetc-focal-ros1-locomotion hhcmhub/kyon-cetc-focal-ros1-locomotion:$TAGNAME
 
 
-export KERNEL_VER=6
-docker compose build 
-docker tag kyon-cetc-focal-ros1-xeno hhcmhub/kyon-cetc-focal-ros1-xeno-v$KERNEL_VER:latest
+# export KERNEL_VER=6
+# docker compose build 
+# docker tag kyon-cetc-focal-ros1-xeno hhcmhub/kyon-cetc-focal-ros1-xeno-v$KERNEL_VER:$TAGNAME
+# docker push hhcmhub/kyon-cetc-focal-ros1-xeno-v6:latest
 
-docker push hhcmhub/kyon-cetc-focal-ros1-base:latest
-docker push hhcmhub/kyon-cetc-focal-ros1-xeno-v5:latest
-docker push hhcmhub/kyon-cetc-focal-ros1-xeno-v6:latest
-docker push hhcmhub/kyon-cetc-focal-ros1-locomotion:latest
+docker push hhcmhub/kyon-cetc-focal-ros1-base:$TAGNAME
+docker push hhcmhub/kyon-cetc-focal-ros1-xeno-v5:$TAGNAME
+docker push hhcmhub/kyon-cetc-focal-ros1-locomotion:$TAGNAME
--- docker/_build/kyon-cetc-focal-ros1/scripts/build-base.bash
+++ docker/_build/kyon-cetc-focal-ros1/scripts/build-base.bash
@@ -16,3 +16,12 @@ forest grow kyon_config -j8 -v
 forest grow xbot2_gui_server
 forest grow vectornav -j8
 forest grow xbot2_cli -j8
+forest grow pybind11
+forest grow centauro_cartesio -j8
+forest grow xbot2_tools -j8
+forest grow pybind11
+forest grow cartesio_collision_support -j8
+forest grow hesai_ros_driver -j8
+
+# rm build to save space
+rm -rf build
