fb72a68055406e32575294c707bafb03e13fb0e3
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-05-09 13:04:03 +0200

    add dialout, fix ros master

--- host/control_profile.bash
+++ host/control_profile.bash
@@ -2,6 +2,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # ros config
 export ROS_IP=10.24.12.102
+export ROS_MASTER_URI=http://10.24.12.100:11311
 
 # enable ros1 alias to login into docker container
 source $SCRIPT_DIR/../docker/kyon-cetc-focal-ros1/setup.sh
