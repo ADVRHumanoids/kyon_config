b9b6706e4b7cfa819b2f7b09495838014986b497
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-05-15 16:02:39 +0200

    argcomplete support in env

--- docker/_build/kyon-cetc-focal-ros1/scripts/env.bash
+++ docker/_build/kyon-cetc-focal-ros1/scripts/env.bash
@@ -5,4 +5,8 @@ source /opt/xbot/setup.sh
 source ~/xbot2_ws/setup.bash    
 source ~/xbot2_ws/src/kyon_config/setup.sh
 
+eval "$(register-python-argcomplete ecat)"
+eval "$(register-python-argcomplete forest)"
+eval "$(register-python-argcomplete concert_laucher)"
+
 export PS1="${CUSTOM_PS}${PS1}"
