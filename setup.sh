893f380d4f0de6d236cc886dc8d10aabeb525ddd
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-05-15 16:02:53 +0200

    concert launcher default config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# ecat 
 export ECAT_MASTER_CONFIG="$SCRIPT_DIR/ecat/ecat_config.yaml"
-#export ROS_PACKAGE_PATH=$SCRIPT_DIR:$ROS_PACKAGE_PATH
-#alias ecat_master="stdbuf --output=L --error=L repl -f $ECAT_MASTER_CONFIG 2>&1 | tee /tmp/ecat-output"
 alias ecat_master="repl -f $ECAT_MASTER_CONFIG"
 alias ecat_master_gdb="gdb --args repl -f $ECAT_MASTER_CONFIG"
+
+# concert launcher 
+export CONCERT_LAUNCHER_DEFAULT_CONFIG=$SCRIPT_DIR/gui/ros1/launcher_config.yaml
