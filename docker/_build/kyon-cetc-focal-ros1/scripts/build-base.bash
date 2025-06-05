85af9b6f321b73215cc44215af8dfe9dcac51fa6
JVALPASS <val1212.pass@gmail.com>
2025-06-05 11:54:33 +0200

    - Parameterizes the `forest add-recipes` step using the     `ROBOT_RECIPES_BRANCH` variable (with a default to `master`). - Removed some specific `forest grow` commands for more generic package     installation.

--- docker/_build/kyon-cetc-focal-ros1/scripts/build-base.bash
+++ docker/_build/kyon-cetc-focal-ros1/scripts/build-base.bash
@@ -10,18 +10,14 @@ pip install -U protobuf
 mkdir xbot2_ws && cd xbot2_ws
 forest init
 source setup.bash
-forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git -t kyon-cetc
-forest grow iit-kyon-ros-pkg -j8 -v
-forest grow kyon_config -j8 -v
+forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git -t ${ROBOT_RECIPES_BRANCH:-master}
 forest grow xbot2_gui_server
-forest grow vectornav -j8
 forest grow xbot2_cli -j8
 forest grow pybind11
 forest grow centauro_cartesio -j8
 forest grow xbot2_tools -j8
 forest grow pybind11
 forest grow cartesio_collision_support -j8
-forest grow hesai_ros_driver -j8
 
 # rm build to save space
 rm -rf build
