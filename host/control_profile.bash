923cf6031c46814f95eafc1a04d9bbf5f6d8cec5
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-08-01 15:45:17 +0200

    fix ip control pc

--- host/control_profile.bash
+++ host/control_profile.bash
@@ -2,7 +2,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # ros config
 export ROS_IP=10.24.13.102
-export ROS_MASTER_URI=http://10.24.12.100:11311
+export ROS_MASTER_URI=http://10.24.13.100:11311
 
 # enable ros1 alias to login into docker container
 source $SCRIPT_DIR/../docker/kyon-cetc-focal-ros1/setup.sh
