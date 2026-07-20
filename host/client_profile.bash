4215b2c6ccd8ff1955fff22d74dfe5ce3546f2fc
Arturo Laurenzi <arturo.laurenzi@iit.it>
2026-07-20 10:44:04 +0200

    fix client profile

--- host/client_profile.bash
+++ host/client_profile.bash
@@ -1,4 +1,4 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+_KYON_CLIENT_PROFILE_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # ssh aliases
 alias ssh_embedded='ssh embedded@10.24.15.100'
@@ -6,7 +6,7 @@ alias ssh_control='ssh kyon@10.24.15.102'
 alias ssh_vision='ssh kyon@10.24.15.101'
 
 # enable ros2 alias to login into docker container
-source $SCRIPT_DIR/../docker/kyon-noble-ros2/setup.sh
+source $_KYON_CLIENT_PROFILE_SCRIPT_DIR/../docker/kyon-noble-ros2/setup.sh
 
 # specific ros domain id for clients to avoid any overlap with the local ros2 network
 export ROS_DOMAIN_ID=42
@@ -16,15 +16,14 @@ export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
 
 # connect to the robot's vpn via wireguard
 function kyon_connect_wg() {
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-    $SCRIPT_DIR/../network/wg/connect-wg.bash "$@"
+    $_KYON_CLIENT_PROFILE_SCRIPT_DIR/../network/wg/connect-wg.bash "$@"
 }
 
 # enable zenoh bridge 
 function kyon_connect_zenoh() {
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-    cd $SCRIPT_DIR/../docker/kyon-noble-ros2 
+    cd $_KYON_CLIENT_PROFILE_SCRIPT_DIR/../docker/kyon-noble-ros2 
     docker compose up -d zenoh_bridge_client
+    cd -
 }
 
 # do it all
