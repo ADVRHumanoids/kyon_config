86f7df3c3540fa5b86b6c34bd536c312a3367237
Your Name <you@example.com>
2026-09-23 16:15:27 +0200

    jetson cnfigs

---
+++ docker/perception_profile_docker.bash
@@ -0,0 +1,7 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# generic
+source $SCRIPT_DIR/generic_profile_docker.bash
+
+# cyclonedds config
+export CYCLONEDDS_URI=file://$SCRIPT_DIR/../network/cyclone/cyclonedds-perception.xml
---
+++ host/perception_profile.bash
@@ -0,0 +1,21 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# ros config
+export ROS_IP=10.24.15.101
+export ROS_MASTER_URI=http://10.24.15.100:11311
+
+# enable ros2 alias to login into docker container
+#source $SCRIPT_DIR/../docker/kyon-noble-ros2/setup.sh
+
+# configure zenoh bridge to listen on all interfaces (default)
+export ZENOH_BRIDGE_ARGS=""
+
+# ssh aliases
+alias ssh_embedded='ssh embedded@amax-kyon-iit'
+alias ssh_control='ssh kyon@kyon-control'
+
+# config path
+export CONFIG_PATH_DOCKER=docker/perception_profile_docker.bash
+
+# set concert launcher output folder
+export CONCERT_LAUNCHER_STDOUT_PATH=/var/log/concert_launcher
